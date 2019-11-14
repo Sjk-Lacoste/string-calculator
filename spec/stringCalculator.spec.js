e05ae7246df12028a5ddb0476473a506447bccb7
Sjk-Lacoste <tmohlatlole@gmail.com>
2019-11-14 11:33:00 +0200

    Uncommented my tests

--- spec/stringCalculator.spec.js
+++ spec/stringCalculator.spec.js
@@ -4,28 +4,28 @@ let strCalc = new str;
 
 console.log(strCalc.Add("-//1\n-2,101%-3"));
 
-// describe("StringCalculator()", () => {
-// 	it("should should return zero if the string is empty", () => {
-// 		expect(strCalc.Add("")).toEqual(0);
-// 	});
+describe("StringCalculator()", () => {
+	it("should should return zero if the string is empty", () => {
+		expect(strCalc.Add("")).toEqual(0);
+	});
 
-// 	it("should be able to add two string values", () => {
-// 		expect(strCalc.Add("1,2")).toEqual(3);
-// 	});
+	it("should be able to add two string values", () => {
+		expect(strCalc.Add("1,2")).toEqual(3);
+	});
 
-// 	it("should return result if input is: '1\n2,3'", () => {
-// 		expect(strCalc.Add("1\n2,3")).toEqual(6);
-//     });
+	it("should return result if input is: '1\n2,3'", () => {
+		expect(strCalc.Add("1\n2,3")).toEqual(6);
+    });
     
-//     it("should support different delimeters", () => {
-//         expect(strCalc.Add("//;n1;2")).toEqual(3);
-//     });
+    it("should support different delimeters", () => {
+        expect(strCalc.Add("//;n1;2")).toEqual(3);
+    });
 
-//     it("should ignore numbers bigger than 1000", () => {
-//         expect(strCalc.Add("2,1001")).toEqual(2);
-//     });
+    it("should ignore numbers bigger than 1000", () => {
+        expect(strCalc.Add("2,1001")).toEqual(2);
+    });
 
-//     it("should support delimeters of any length", () => {
-//         expect(strCalc.Add("//[**][%%]\n1**2%%3")).toEqual(6);
-//     });
-// });
+    it("should support delimeters of any length", () => {
+        expect(strCalc.Add("//[**][%%]\n1**2%%3")).toEqual(6);
+    });
+});
