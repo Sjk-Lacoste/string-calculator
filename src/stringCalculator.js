b3a1d2b384489b1fc238fd3ff8094743ed151651
Sjk-Lacoste <tmohlatlole@gmail.com>
2019-11-14 11:31:44 +0200

    Completed everything

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -2,19 +2,40 @@
 
 module.exports = class StringCalculator {
     constructor() {
-        this.reg = /[\n\,]/;
+        this.reg = /\d{1,}/gm;
+
+        this.extractNegative = (string) => {
+            let str = '';
+            for(let i = 0; i < string.length; i++) {
+                if(string[i] == '-' && !isNaN(string[i+1])) {
+                    str += string[i+1] + ', ';
+                }
+            }
+            return str;
+        }
     }
 
     Add(string) {
         let sum = 0;
-        let str = string.split(this.reg);
+        let str = string.match(this.reg, '');
+        let negatives = this.extractNegative(string);
+        
+        try {
+            if(string.includes("-")) throw ("negative " + negatives + "not allowed.");
+        } catch (err) {
+            return err;
+        }
 
-        if(str == "") {
+        if(string == "") {
             return 0;
         } else {
             for(let i = 0; i < str.length; i++) {
-                let num = parseInt(str[i]);
-                sum += num;
+                if(str[i] >= 1000){
+                    continue;
+                } else {
+                    let num = parseInt(str[i]);
+                    sum += num;
+                }
             }
 
             return sum;
